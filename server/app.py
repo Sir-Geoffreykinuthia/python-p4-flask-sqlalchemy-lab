52548566ac06b607ce26671dc7119dff241166c1
geoffrey kinuthia <geoffreykinuthia10@gmail.com>
2023-06-21 13:20:10 +0300

    First commit

--- server/app.py
+++ server/app.py
@@ -19,16 +19,53 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+
+    return f'''
+     <ul>
+        <li>ID: {animal.id}</li>
+        <li>Name: {animal.name}</li>
+        <li>Species: {animal.species}</li>
+        <li>Zookeeper: {animal.zookeeper_id}</li>
+        <Li>Enclosure: {animal.enclosure_id}</li>
+     </ul> 
+    '''
+
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    animals = ''
+    for animal in zookeeper.animals:
+        animals += f'<ul>Animal: {animal.name}</ul>'
+    return f'''
+        <ul>
+        <li>ID: {zookeeper.id}</li>
+        <li>Name: {zookeeper.name}</li>
+        <li>Birthday: {zookeeper.birthday}</li>
+        </ul> 
+        <ul>
+        {animals}
+        </ul>
+    '''
+
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
-
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    animals = ''
+    for animal in enclosure.animals:
+        animals += f'<ul>Animal: {animal.name}</ul>'
+    return f'''
+        <ul>
+        <li>ID: {enclosure.id}</li>
+        <li>Environment: {enclosure.environment}</li>
+        <li>Open to visitors: {enclosure.open_to_visitors}</li>
+        </ul>
+        <ul>
+        {animals}
+        </ul>
+    '''
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
